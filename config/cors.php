8f93af17c6d9b582fc11253f8268a2bd4b7aded2
veeduria-ui <veeduriacsdc@gmail.com>
2025-09-25 07:08:42 -0500

    Update CORS configuration for Azure frontend

--- config/cors.php
+++ config/cors.php
@@ -26,6 +26,8 @@ return [
         'http://127.0.0.1:5174',
         'http://localhost:3000',
         'http://127.0.0.1:3000',
+        'https://csdt-frontend-app.azurewebsites.net',
+        'https://csdt-frontend-app.azurewebsites.net/',
     ],
 
     'allowed_origins_patterns' => [],
